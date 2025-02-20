7f1a5dca2fce415024975249d3e8939fe8803088
Parikshith-27 <parikshithny.mca24@bmsce.ac.in>
2025-02-20 15:11:28 +0530

    changed class

--- mind-quest/src/App.js
+++ mind-quest/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Basic PAT">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/test.js</code> and save to reload.
         </p>
         <a
           className="App-link"
